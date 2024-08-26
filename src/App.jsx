e4990b8da39f63a231ea334be1fdc46c2f00c63e
Muhammad Awais <muhammad.awais@royalcyber.com>
2024-08-26 17:17:27 +0500

    added routing and components for code seprations

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
+import { Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/homepage'
 import Samples from './pages/samples'
 
 function App() {
   return (
     <>
-      <h1>React + GSAP</h1>
+      <div className='d-flex justify-content-between align-items-center'>
+        <h1>React + GSAP</h1>
 
-      <Home />
-      <Samples />
+        <nav>
+          <ul className='d-flex list-unstyled'>
+            <li className='ps-4'><Link to="/">Home</Link></li>
+            <li className='ps-4'><Link to="/samples">Samples</Link></li>
+          </ul>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/samples" element={<Samples />} />
+      </Routes>
     </>
   )
 }
